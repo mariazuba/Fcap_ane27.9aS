02ccb5d339c4ea0d54b92dd72cb2a3de27dfa939
mariazuba <mariazuba@gmail.com>
2026-06-22 13:17:16 +0200

    CODIGO PARA CORRER FLBEIA DESDE CESGA

--- cesga/run.sh
+++ cesga/run.sh
@@ -1,13 +1,11 @@
-
-```bash
 #!/bin/bash
-#SBATCH --job-name=ane_fcap
+#SBATCH --job-name=ane_fcap_30y_1000i
 #SBATCH --output=outputs/mse/logs/sc_%A_%a.out
 #SBATCH --error=outputs/mse/logs/sc_%A_%a.err
 
-#SBATCH --time=04:00:00
+#SBATCH --time=72:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8G
+#SBATCH --mem=32G
 
 #SBATCH --array=1-5
 
@@ -16,11 +14,16 @@ module load R/4.2.2
 
 cd $SLURM_SUBMIT_DIR
 
+mkdir -p outputs/mse/logs
+mkdir -p outputs/mse/res
+mkdir -p outputs/mse/summary
+
 echo "======================================="
 echo "JOB ID: $SLURM_JOB_ID"
 echo "TASK ID: $SLURM_ARRAY_TASK_ID"
 echo "NODE: $(hostname)"
 echo "START: $(date)"
+echo "WORKDIR: $(pwd)"
 echo "======================================="
 
 Rscript scripts/model.R $SLURM_ARRAY_TASK_ID
@@ -28,4 +31,3 @@ Rscript scripts/model.R $SLURM_ARRAY_TASK_ID
 echo "======================================="
 echo "END: $(date)"
 echo "======================================="
-```
